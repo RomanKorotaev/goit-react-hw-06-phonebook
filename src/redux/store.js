422c1b1b2471a3e79e0df9f2035d48afbe334271
RomanKorotaev <kro1986@ukr.net>
2021-11-19 21:37:14 +0200

    Добавлена запись контактов в   localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,17 @@
 import {combineReducers, createStore} from "redux";
+import db from '../db.json'
+
+// Записываем стартовые значения контактов в localStorage
+localStorage.setItem('contactsLocalSt_db',   JSON.stringify( [...db] ) );
 
 const initialState ={
-     contacts: [
-                    {id:"id-1", name: "Rosie Simpson", number: "459-12-56"},
-                    {id:"id-2", name: "Hermione Kline", number: "443-89-12"},
-                    {id:"id-3", name: "Eden Clements", number: "645-17-79"},
-                    {id:"id-4", name: "Annie Copeland", number: "227-91-26"}
-                ],
+    //  contacts: [
+    //                 {id:"id-1", name: "Rosie Simpson", number: "459-12-56"},
+    //                 {id:"id-2", name: "Hermione Kline", number: "443-89-12"},
+    //                 {id:"id-3", name: "Eden Clements", number: "645-17-79"},
+    //                 {id:"id-4", name: "Annie Copeland", number: "227-91-26"}
+    //             ],
+                contacts: JSON.parse(localStorage.getItem('contactsLocalSt_db')),
                 filterValue:'',
             }
     
@@ -21,16 +26,24 @@ console.log (' Лог action  в reducer: ', action)
         case  'contact/add':
             console.log ("Консоль из Свича [...state.contacts, action.payload]", [...state.contacts, action.payload])
             
-            console.log ('TEST: state ', state )
+            console.log ('TEST: state ', state );
+
+            //Дописываем новый контакт в localStorage
+            localStorage.setItem('contactsLocalSt_db',   JSON.stringify( [...state.contacts, action.payload] ) );
+
             return {...state,
                 contacts: [...state.contacts, action.payload]
             }            
      
 // Удаление контакта
         case  'contact/delete':
-                                const tmpArr= state.contacts.filter (oneContact =>{
-                                return oneContact.id !== action.payload
-                            })
+                const tmpArr= state.contacts.filter (oneContact =>{
+                    return oneContact.id !== action.payload
+                })
+
+                //Удаляем в localStorage выбранный контакт 
+            localStorage.setItem('contactsLocalSt_db',   JSON.stringify( [...tmpArr] ) );
+
             return {...state,
                 contacts: [...tmpArr]
             };
